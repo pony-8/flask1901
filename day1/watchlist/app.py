fecadd20f637d9221297e50e381516ff3a7bde4d
pony-8 <2604665482@qq.com>
2020-04-01 19:25:29 +0800

    模板展示

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+# @app.route('/index')
+# @app.route('/home')
 
 def index():
-    
-    return "<h1>hello</h1>"
+    name = "中国"
+    movies = [
+        { "title" : "大赢家" , "year" : "2020" },
+        { "title" : "叶问四" , "year" : "2020" },
+        { "title" : "唐人街探案" , "year" : "2020" },
+        { "title" : "囧妈" , "year" : "2020" },
+    ]
+    return render_template('index.html',name=name,movies=movies)
+
+# # 动态url
+# @app.route('/index/<name>')
+# def home(name):
+#     return "<h1>hello,Flask,%s</h1>" %name
