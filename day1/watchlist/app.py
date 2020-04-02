d72e00a503834f5c94cbd8047cecf216f5bc50d5
pony-8 <2604665482@qq.com>
2020-04-02 11:43:30 +0800

    数据库

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,7 +1,40 @@
+import os
+import sys
+import click
+
 from flask import Flask
 from flask import render_template
+#导入扩展类
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
 app = Flask(__name__)
 
+#Windows下写法
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+
+#关闭了修改模型的监控
+app.config['SQLALCHY_TRACK_MODIFICATIONS'] = False
+
+#初始化扩展，传入程序实例app
+db = SQLAlchemy(app)
+
+#models.py
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+#views.py
 @app.route('/')
 # @app.route('/index')
 # @app.route('/home')
@@ -20,3 +53,16 @@ def index():
 # @app.route('/index/<name>')
 # def home(name):
 #     return "<h1>hello,Flask,%s</h1>" %name
+
+#自定义指令
+@app.cli.command()  #装饰器，可以注册命令
+@click.option('--drop',is_flag=True,help="删除后再创建")
+
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化数据库完成")    
+    
+
+     
